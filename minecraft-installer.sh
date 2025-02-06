8a28406109a6034702034d0b5f5b221d45736fc8
GNU/Alex <pop.alex.rs@proton.me>
2025-02-06 13:03:56 +0100

    Update minecraft-installer.sh
    Modified installation path to HOME directory

--- minecraft-installer.sh
+++ minecraft-installer.sh
@@ -3,15 +3,16 @@
 filename="minecraft.tar.gz"
 filename_icon="minecraft-launcher.png"
 url="https://launcher.mojang.com/download/Minecraft.tar.gz"
-launcher_icon_url="https://www.minecraft.net/etc.clientlibs/minecraft/clientlibs/main/resources/img/Launcher_Icon.png"
-desktop_file="/usr/share/applications/minecraft-launcher.desktop"
+install_dir="$HOME/.mojang"
+desktop_file="$HOME/.local/share/applications/minecraft-launcher.desktop"
 
+# Create the installation directory if it doesn't exist
+mkdir -p "$install_dir"
 
 echo "Downloading..."
 # Getting Minecraft launcher from "Other" option on page minecraft.net/download
 wget -q --show-progress -O $filename $url
 echo "Download complete."
-
 
 # Decompressing minecraft.tar.gz
 echo "Decompressing $filename"
@@ -20,35 +21,30 @@ echo "Decompressing done."
 
 # Moving archive
 echo "Moving archive..."
-sudo mv minecraft-launcher/ /opt/
+mv minecraft-launcher/ "$install_dir/"
 echo "Moving launcher Icon..."
-sudo mv $filename_icon /opt/minecraft-launcher/ 
+mv $filename_icon "$install_dir/"
 echo "Moving complete..."
 
 # Creating .desktop file
 echo "Creating .desktop file..."
-sudo bash -c "cat > $desktop_file << EOF
+cat > "$desktop_file" << EOF
 [Desktop Entry]
 Type=Application
 Version=1.0
 Name=Minecraft Launcher
 Comment=Official Minecraft Launcher
-Exec=/opt/minecraft-launcher/minecraft-launcher
+Exec=$install_dir/minecraft-launcher/minecraft-launcher
 # If you have a laptop with Nvidia GPU, use this line for launching Minecraft using your GPU.
-#Exec=env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia /opt/minecraft-launcher/minecraft-launcher
-Icon=/opt/minecraft-launcher/minecraft-launcher.png
-#Use this line for using the Minecraft icon from your Icons theme.
-#Icon=minecraft
+#Exec=env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $install_dir/minecraft-launcher/minecraft-launcher
+Icon=$install_dir/minecraft-launcher.png
 Terminal=false
 Categories=Game;Application;
 StartupWMClass=minecraft-launcher
-EOF"
+EOF
 
 echo ".desktop file created at $desktop_file"
 
-#Removing .tar.gz archive
+# Removing .tar.gz archive
 echo "Removing $filename"
 rm $filename
-
-
-
