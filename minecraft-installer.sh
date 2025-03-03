71533b9d99f327b067182d34bca216a7edb8707b
GNU/Alex <pop.alex.rs@proton.me>
2025-03-03 12:39:53 +0100

    Update minecraft-installer.sh
    Improving the script

--- minecraft-installer.sh
+++ minecraft-installer.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
+#!/bin/bash
+# =====================================
+# ======== Minecraft Installer ========
+# =====================================
+# Version : 1.1
+# Author : GNU/Alex
+# Description : Script for installing Minecraft
+# on non-Debian/Arch Distributions.
+# =====================================
 
-filename="Minecraft.tar.gz"
-filename_icon="minecraft-launcher.png"
+
+filename="minecraft.tar.gz"
+filename_icon="minecraft-launcher.svg"
 url="https://launcher.mojang.com/download/Minecraft.tar.gz"
-install_dir="$HOME/.mojang"
+install_dir="$HOME/.local/bin"
+icon_dir="$HOME/.icons/hicolor/symbolic/apps"
 desktop_file="$HOME/.local/share/applications/minecraft-launcher.desktop"
 
 # Create the installation directory if it doesn't exist
 mkdir -p "$install_dir"
+mkdir -p "$icon_dir"
 
 echo "Downloading..."
 # Getting Minecraft launcher from "Other" option on page minecraft.net/download
-wget -q --show-progress -O $filename $url
-wget https://github.com/GNU-Alex/minecraft-installer-linux/raw/main/minecraft-launcher.png
+wget -q --show-progress -O "$filename" "$url"
+wget -q --show-progress -O "$icon_dir/$filename_icon" "https://raw.githubusercontent.com/GNU-Alex/minecraft-installer-linux/main/minecraft-launcher.svg"
+
 echo "Download complete."
 
 # Decompressing minecraft.tar.gz
 echo "Decompressing $filename"
-tar -xzvf $filename
+tar -xzvf "$filename"
 echo "Decompressing done."
 
 # Moving archive
 echo "Moving archive..."
-mv minecraft-launcher/ "$install_dir/"
+mv minecraft-launcher/minecraft-launcher "$install_dir/"
+rm -rf minecraft-launcher/
 echo "Moving launcher Icon..."
-mv $filename_icon "$install_dir/"
+mv "$icon_dir/$filename_icon" "$icon_dir/"
 echo "Moving complete..."
 
 # Creating .desktop file
@@ -35,20 +49,15 @@ Type=Application
 Version=1.0
 Name=Minecraft Launcher
 Comment=Official Minecraft Launcher
-Exec=$install_dir/minecraft-launcher/minecraft-launcher
-# If you have a laptop with Nvidia GPU, use this line for launching Minecraft using your GPU.
-#Exec=env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia $install_dir/minecraft-launcher/minecraft-launcher
-Icon=$install_dir/minecraft-launcher.png
+Exec=$install_dir/minecraft-launcher
+Icon=$icon_dir/$filename_icon
 Terminal=false
 Categories=Game;Application;
-StartupWMClass=Minecraft Launcher
+StartupWMClass=minecraft-launcher
 EOF
 
 echo ".desktop file created at $desktop_file"
 
-# Make the .desktop file executable
-chmod +x "$desktop_file"
-
 # Removing .tar.gz archive
 echo "Removing $filename"
-rm $filename
+rm "$filename"
